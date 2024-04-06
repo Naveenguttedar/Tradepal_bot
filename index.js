61555c68f779d681bf880fb3a7eb5e303ac315fe
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-04-07 00:36:31 +0530

    added server using vercel

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-const TelegramBot = require("node-telegram-bot-api");
-const axios = require("axios");
-const express = require('express');
-const app = express();
+import { parse } from 'url';
+import { json, send } from 'micro';
+import TelegramBot from 'node-telegram-bot-api';
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-const port = 3000;
-app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
-});
-// replace the value below with the Telegram token you receive from @BotFather
 const token = "7106683591:AAFhWQQ2eVLbRlomWOlLRpcvI7eOx7DZ2Xo";
+const bot = new TelegramBot(token, { polling: false }); 
 
-function decodeMsg(text){
-	const cleanedText = text.replace(/\n/g, '');
-const regex = /Currency pair\s*([\w/]+)\s*.*put\s*["']?(UP|DOWN)["']?/i;
+function decodeMsg(text) {
+    const cleanedText = text.replace(/\n/g, '');
+    const regex = /Currency pair\s*([\w/]+)\s*.*put\s*["']?(UP|DOWN)["']?/i;
 
-const match = cleanedText.match(regex);
-	if (match) {
-		const currencyPair = match[1]; // EUR/USD
-		const putOption = match[2];    // UP or DOWN
-		return`Currency Pair: +${currencyPair}+\n+Put Option: +${putOption}`;
-	} else {
-		return("No match found.");
-	}
-
+    const match = cleanedText.match(regex);
+    if (match) {
+        const currencyPair = match[1]; // EUR/USD
+        const putOption = match[2];    // UP or DOWN
+        return `Currency Pair: ${currencyPair} \n Put Option: ${putOption}`;
+    } else {
+        return "No match found.";
+    }
 }
 
-const bot = new TelegramBot(token, { polling: true });
+async function handleRequest(req, res) {
+    const { pathname } = parse(req.url);
 
-bot.on("message", async (msg) => {
-	const chatId = msg.chat.id;
-	const userInput = msg.text||msg.caption;
-	const signal=decodeMsg(msg.caption);
-	bot.sendMessage(chatId,signal);
+    if (pathname === '/webhook') {
+        const body = await json(req);
+        bot.processUpdate(body);
 
-});
+        // Extract the message from the update
+        const message = body.message || body.edited_message;
+        if (message) {
+            const chatId = message.chat.id;
+            const userInput = message.caption || message.text;
+            const signal = decodeMsg(userInput);
+            bot.sendMessage(chatId, signal);
+        }
+
+        send(res, 200, 'OK');
+    } else {
+        send(res, 404, 'Not found');
+    }
+}
+export default handleRequest;
+
